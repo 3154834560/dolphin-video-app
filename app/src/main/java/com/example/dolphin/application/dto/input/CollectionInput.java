925d005b54265d9763ebdae453c63ebed0c27fdc
王景阳 <3154834560@qq.com>
2023-03-30 16:32:22 +0800

    清除CollectionInput多余字段

--- app/src/main/java/com/example/dolphin/application/dto/input/CollectionInput.java
+++ app/src/main/java/com/example/dolphin/application/dto/input/CollectionInput.java
@@ -3,6 +3,8 @@ package com.example.dolphin.application.dto.input;
 import lombok.Data;
 
 /**
+ * 收藏
+ *
  * @author 王景阳
  * @date 2022/11/10 21:08
  */
@@ -13,29 +15,9 @@ public class CollectionInput {
      */
     private String videoId;
     /**
-     * 视频作者，对应用户名
-     */
-    private String author;
-    /**
-     * 头像名称-带后缀
-     */
-    private String headPortraitName;
-    /**
-     * 昵称
-     */
-    private String nick;
-    /**
      * 视频封面-带后缀
      */
     private String coverName;
-    /**
-     * 视频名-带后缀
-     */
-    private String videoName;
-    /**
-     * 视频简介
-     */
-    private String introduction;
     /**
      * 点赞数
      */
